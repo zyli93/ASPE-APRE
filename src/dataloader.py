2fd1f6980ab74ace14de13a7f1fd5caff588ec7d
zyli93 <zyli@cs.ucla.edu>
2020-12-04 23:54:41 -0800

    started work on model

---
+++ src/dataloader.py
@@ -0,0 +1,10 @@
+'''
+    Extracting the terms in the document
+
+    File name: extract.py
+    Author: Zeyu Li 
+    Email: <zyli@cs.ucla.edu> or <zeyuli@g.ucla.edu>
+    Date Created: 11/23/2020
+    Date Last Modified: TODO
+    Python Version: 3.6
+'''
